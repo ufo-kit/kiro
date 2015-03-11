92aefdde688c834dac8380dbd64d98961ee04055
Timo Dritschler <timo.dritschler@kit.edu>
2015-03-11 17:58:04 +0100

    Added basic interface definition for KiroMessenger Added preliminary implementation for KiroMessenger Updated CMake files for KiroMessenger

---
+++ src/kiro-messenger.h
@@ -0,0 +1,258 @@
+/* Copyright (C) 2014-2015 Timo Dritschler <timo.dritschler@kit.edu>
+   (Karlsruhe Institute of Technology)
+
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as published by the
+   Free Software Foundation; either version 2.1 of the License, or (at your
+   option) any later version.
+
+   This library is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+   details.
+
+   You should have received a copy of the GNU Lesser General Public License along
+   with this library; if not, write to the Free Software Foundation, Inc., 51
+   Franklin St, Fifth Floor, Boston, MA 02110, USA
+*/
+
+/**
+ * SECTION: kiro-messenger
+ * @Short_description: KIRO RDMA Messenger
+ * @Title: KiroMessenger
+ *
+ * KiroMessenger implements a generic messenging interface for KIRO RDMA
+ * communication. A messenger can be started either as listening or connecting
+ * side. However, after connecting, both sides are identical in functionality.
+ * Messenger connections are allways only single-point-to-point.
+ */
+
+#ifndef __KIRO_MESSENGER_H
+#define __KIRO_MESSENGER_H
+
+#include <stdint.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define KIRO_TYPE_MESSENGER             (kiro_messenger_get_type())
+#define KIRO_MESSENGER(obj)             (G_TYPE_CHECK_INSTANCE_CAST((obj), KIRO_TYPE_MESSENGER, KiroMessenger))
+#define KIRO_IS_MESSENGER(obj)          (G_TYPE_CHECK_INSTANCE_TYPE((obj), KIRO_TYPE_MESSENGER))
+#define KIRO_MESSENGER_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST((klass), KIRO_TYPE_MESSENGER, KiroMessengerClass))
+#define KIRO_IS_MESSENGER_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE((klass), KIRO_TYPE_MESSENGER))
+#define KIRO_MESSENGER_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS((obj), KIRO_TYPE_MESSENGER, KiroMessengerClass))
+
+
+typedef struct _KiroMessenger           KiroMessenger;
+typedef struct _KiroMessengerClass      KiroMessengerClass;
+typedef struct _KiroMessengerPrivate    KiroMessengerPrivate;
+
+typedef gboolean KiroContinueFlag;
+#define KIRO_CALLBACK_CONTINUE TRUE
+#define KIRO_CALLBACK_REMOVE FALSE
+
+enum KiroMessengerType {
+    KIRO_MESSENGER_SERVER = 0,
+    KIRO_MESSENGER_CLIENT
+};
+
+enum KiroMessageStatus {
+    KIRO_MESSAGE_OKAY = 0,
+    KIRO_MESSAGE_SENT_SUCCESS,
+    KIRO_MESSAGE_SEND_FAILED,
+    KIRO_MESSAGE_RECEIVED
+};
+
+
+struct _KiroMessenger {
+
+    GObject parent;
+
+    /*< private >*/
+    KiroMessengerPrivate *priv;
+};
+
+
+struct _KiroMessengerClass {
+
+    GObjectClass parent_class;
+
+};
+
+struct KiroMessage {
+    enum KiroMessageStatus status; // Status of the message
+    guint64     id;           // Unique ID of the message. This may not be changed by the user
+    guint64     size;         // Size of the messages payload in bytes
+    gpointer    payload;      // Pointer to the payload of the message
+    gboolean    message_handled; // FALSE initially, TRUE once the message was handled
+};
+
+
+/* GObject and GType functions */
+
+/**
+ * kiro_messenger_get_type: (skip)
+ * Returns: GType of KiroMessenger
+ */
+GType        kiro_messenger_get_type            (void);
+
+/**
+ * kiro_messenger_new - Creates a new #KiroMessenger
+ * Returns: (transfer full): A pointer to a new #KiroMessenger
+ * Description:
+ *   Creates a new, unbound #KiroMessenger and returns a pointer to it.
+ * See also:
+ *   kiro_messenger_start, kiro_messenger_free
+ */
+KiroMessenger*  kiro_messenger_new                (void);
+
+/**
+ * kiro_messenger_free - 'Destroys' the given #KiroMessenger
+ * @messenger: The #KiroMessenger that is to be freed
+ * Description:
+ *   Transitions the #KiroMessenger through all necessary shutdown routines and
+ *   frees the object memory.
+ * See also:
+ *   kiro_messenger_start, kiro_messenger_new
+ */
+void         kiro_messenger_free                (KiroMessenger *messenger);
+
+
+/* messenger functions */
+
+/**
+ * kiro_messenger_start - Starts the messenger
+ * @messenger: #KiroMessenger to perform the operation on
+ * @bind_addr: Local address to bind the messenger to
+ * @bind_port: Local port to listen for connections
+ * @role: A #KiroMessengerType which is used to decide the role of this
+ * messenger 
+ * Returns: An integer denoting success of this function. 0 for success, -1
+ * otherwise
+ * Description:
+ *   Starts the #KiroMessenger with the given role. When @role is given as
+ *   KIRO_MESSENGER_SERVER, the messenger will open an InfiniBand connection and
+ *   listen for the first client that tries to connect. When given
+ *   KIRO_MESSENGER_CLIENT, the messenger will instead try to connect to to the
+ *   given address.
+ * Notes:
+ *   If the bind_addr is NULL, the messenger will bind to the first device
+ *   it can find on the machine and listen across all IPs. Otherwise it
+ *   will try to bind to the device associated with the given address.
+ *   Address is given as a string of either a hostname or a dot-separated
+ *   IPv4 address or a colon-separated IPv6 hex-address.
+ *   If bind_port is NULL the messenger will choose a free port randomly
+ *   and return the chosen port as return value.
+ * See also:
+ *   kiro_messenger_new,
+ */
+int kiro_messenger_start (KiroMessenger *messenger, const char *bind_addr, const char *bind_port, enum KiroMessengerType role);
+
+
+/**
+ * KiroReceiveCallbackFunc - Function type for sync callbacks of a
+ * #KiroMessenger
+ * @message: A pointer to the #KiroMessage that was received and/or sent
+ * @user_data: (transfer none): The #user_data which was provided during
+ * registration of this callback
+ * Returns: A #KiroContinueFlag
+ * Description:
+ *   Defines the type of a callback function which will be invoked every time
+ *   the messenger has sent received a message
+ * Note:
+ *   Returning %FALSE or %KIRO_CALLBACK_REMOVE will automatically remove the callback
+ *   from the internal callback list. Return %TRUE or %KIRO_CALLBACK_CONTINUE if you
+ *   want to keep the callback active.
+ *   A receive callback function needs to set the "message_handled" flag correctly, if
+ *   it has taken the messages content and set the "payload" pointer in the
+ *   message struct to %NULL. Otherweise, the messages payload will be freed
+ *   once all callbacks were invoked.
+ *   If the callback receives a message which already has the "message_handled"
+ *   flag set, it can safely ignore the message and just return.
+ * See also:
+ *   kiro_messenger_add_receive_callback, kiro_messenger_remove_receive_callback,
+ *   kiro_messenger_add_send_callback, kiro_messenger_remove_send_callback
+ */
+typedef KiroContinueFlag (*KiroMessengerCallbackFunc)   (struct KiroMessage *message, void *user_data);
+
+/**
+ * kiro_messenger_add_receive_callback - Register a callback function for
+ * receive events
+ * @messenger: #KiroMessenger to perform this operation on
+ * @callback: Pointer to a #KiroReceiveCallbackFunc that will be invoked when a
+ * messege is received
+ * @user_data: Pointer to user data that will be passed to the callback function
+ * Returns: Integer ID of the registered callback. -1 in case of error
+ * Description:
+ *   Registers the given callback function to be invoked every time the
+ *   messenger receives a message.
+ * See also:
+ *   kiro_messenger_remove_receive_callback
+ */
+int kiro_messenger_add_receive_callback (KiroMessenger *messenger, KiroMessengerCallbackFunc *callback, void *user_data);
+
+/**
+ * kiro_messenger_remove_receive_callback - Removes a previously registered
+ * callback
+ * @messenger: #KiroMessenger to perform this operation on
+ * @callback_id: ID of the callback that should be removed
+ * Returns: 0 on success -1 in case of error
+ * Description:
+ *   Removes the callback with the given ID.
+ * See also:
+ *   kiro_messenger_add_receive_callback
+ */
+int kiro_messenger_remove_receive_callback (KiroMessenger *messenger, int callback_id);
+
+/**
+ * kiro_messenger_add_send_callback - Register a callback function for
+ * send completions
+ * @messenger: #KiroMessenger to perform this operation on
+ * @callback: Pointer to a #KiroSendCallbackFunc that will be invoked when a
+ * messege was sent
+ * @user_data: Pointer to user data that will be passed to the callback function
+ * Returns: Integer ID of the registered callback. -1 in case of error
+ * Description:
+ *   Registers the given callback function to be invoked every time the
+ *   messenger has sent a message
+ * See also:
+ *   kiro_messenger_remove_send_callback
+ */
+int kiro_messenger_add_send_callback (KiroMessenger *messenger, KiroMessengerCallbackFunc *callback, void *user_data);
+
+/**
+ * kiro_messenger_remove_send_callback - Removes a previously registered
+ * callback
+ * @messenger: #KiroMessenger to perform this operation on
+ * @callback_id: ID of the callback that should be removed
+ * Returns: 0 on success -1 in case of error
+ * Description:
+ *   Removes the callback with the given ID.
+ * See also:
+ *   kiro_messenger_send_receive_callback
+ */
+int kiro_messenger_remove_send_callback (KiroMessenger *messenger, int callback_id);
+
+/**
+ * kiro_messenger_send - send the given message to the remote side
+ * @messenger: #KiroMessenger to use for sending the message
+ * @message: Pointer to a #KiroMessage for sending
+ * Returns: 0 on success, -1 in case of error
+ * Description:
+ *   Sends the given #KiroMessage to the remote side
+ */
+int kiro_messenger_send_message (KiroMessenger *messenger, struct KiroMessage *message);
+
+/**
+ * kiro_messenger_stop - Stops the messenger
+ * @messenger: #KiroMessenger to perform the operation on
+ * Description:
+ *   Stops the given #KiroMessenger
+ * See also:
+ *   kiro_messenger_start
+ */
+void kiro_messenger_stop (KiroMessenger *messenger);
+
+G_END_DECLS
+
+#endif //__KIRO_MESSENGER_H
