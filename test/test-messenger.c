b2f03298339b4a2a6beaac237b9eaf953c5320be
Timo Dritschler <timo.dritschler@kit.edu>
2015-03-18 18:33:29 +0100

    Extended kiro-test-messenger to be able to pass commandline strings for testing

--- test/test-messenger.c
+++ test/test-messenger.c
@@ -18,29 +18,52 @@ grab_message (struct KiroMessage *msg, gpointer user_data)
 int
 main ( int argc, char *argv[] )
 {
-    if (argc < 3) {
-        printf ("Not enough aruments. Usage: kiro-test-bandwidth <address> <port>\n");
+    GOptionContext *context;
+    GError *error = NULL;
+
+    static gboolean server = FALSE;
+
+    static GOptionEntry entries[] = {
+        { "server", 's', 0, G_OPTION_ARG_NONE, &server, "Start as server (listener)", NULL },
+        { NULL }
+    };
+
+#if !(GLIB_CHECK_VERSION (2, 36, 0))
+    g_type_init ();
+#endif
+
+    context = g_option_context_new ("[-s | <ADDRESS>] <MESSAGE>");
+    g_option_context_set_summary (context, "Send the supplied message to a peer KiroMessenger");
+    g_option_context_add_main_entries (context, entries, NULL);
+
+    if (!g_option_context_parse (context, &argc, &argv, &error)) {
+        g_print ("Error parsing options: %s\n", error->message);
         return -1;
+    }
+
+    if (argc < 2 && !server) {
+        g_print ("%s", g_option_context_get_help (context, TRUE, NULL));
+        return 0;
     }
 
     KiroMessenger *messenger = kiro_messenger_new ();
 
-    enum KiroMessengerType type = (argc > 3) ? KIRO_MESSENGER_SERVER : KIRO_MESSENGER_CLIENT;
+    enum KiroMessengerType type = (server) ? KIRO_MESSENGER_SERVER : KIRO_MESSENGER_CLIENT;
 
-    if (-1 == kiro_messenger_start (messenger, argv[1], argv[2], type)) {
+    if (-1 == kiro_messenger_start (messenger, argv[1], "60010", type)) {
         kiro_messenger_free (messenger);
         return -1;
     }
 
     if (type == KIRO_MESSENGER_CLIENT) {
         struct KiroMessage msg;
-        msg.payload = g_malloc0 (20);
-        strcpy (msg.payload, "Hello World!");
-        msg.size = 13;
+        GString *str = g_string_new (argv[2]);
+        msg.payload = str->str;
+        msg.size = str->len + 1; // respect the NULL byte
         if (0 > kiro_messenger_send_message (messenger, &msg))
             printf ("Sending failed...");
         else
-            printf ("Message sent successfully\n");
+            printf ("Message submitted successfully\n");
         sleep (3);
     }
     else {
